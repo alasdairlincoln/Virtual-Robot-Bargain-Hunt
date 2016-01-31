5dca13a0c952b3869754a26fa864767ef0be9e01
Drakoshi <taduksaz@live.com>
2016-01-31 23:23:37 +0000

    Discuss points, Comments, Changes

--- Main.py
+++ Main.py
@@ -1,34 +1,35 @@
 import sys
 from tkinter import *
 
-def ReadSplit(filePath):
-    """Opens specified file, reads it and splits into single symbols"""
+"""Discuss the further development of the program
+   Should we make a wrapper for most of the stuff we use, or just the main stuff that is going to be needed for sure like canvas for map...
+   I feel like create rectangle and other shapes that will be in the game is needed since we need to make the rectangles the same size
+   the button is not rly needed... after much thinking
+   move object is good, needs more wrapper layer to respond to button presses and move accordingly
+   root should not be used, put everything into the frame which is acuired with gui.GetMainFrame()"""
 
-    file = open(filePath,"r")
-    string = file.read()
-    file.close()
-
-    lPre = string.rsplit("\n") # l stands for list
-    lFinal = []
-
-    for i in lPre:
-        lFinal.append(i.split())
-
-    return lFinal
+"""Tasks:
+   Make W/A/S/D or arrow keys move the cat
+   Make cat class
+   join implement main menu"""
 
 class GUI:
 
     def __init__(self,root):
         self.__root = root
+        self.__frame = Frame(self.__root).pack()
 
         # change window info here!
         self.__root.title("Cat Hunt")
-        self.__wWidth = 250
-        self.__wHeight = 250
+        self.__wWidth = 500
+        self.__wHeight = 500
         self.__Div = 50
         # ------------------------
 
-        self.__canvas = Canvas(self.__root,width = self.__wWidth, height = self.__wHeight, bg = "white")
+        # Insert start/main menu here? make it return some stuff to here?
+
+        # Canvas for map
+        self.__canvas = Canvas(self.__frame,width = self.__wWidth, height = self.__wHeight, bg = "white")
         self.__canvas.pack()
 
     def CreateRectangle(self,x,y,Fill = "white",Outline = "black", returnOn = False):
@@ -40,68 +41,101 @@ class GUI:
         if returnOn == True:
             return rect
 
-    def __actionSelect(self,number,pArgs):
+    def __ActionSelect(self,number,pArgs):
         # used as a place holder to get other commands, since i failed to pass in a command :| + google suggested this :D 
         if number == 0:
             # default case in case nothing specified
             pass
-        if number == 1:
+        elif number == 1:
             # unpack and pass arguments 
             ID = pArgs[0]
             x = pArgs[1]
             y = pArgs[2]
             self.MoveObject(ID,x,y)
 
-    def CreateButton(self, txt,ComN = 0,comArgs = ()):
-        button = Button(self.__root,text = txt,command = lambda: self.__actionSelect(ComN,comArgs))
-        button.pack()
+    def CreateButton(self, txt, ComN = 0, comArgs = (), Side = "top", returnOn = False):
+        button = Button(self.__frame,text = txt,command = lambda: self.__ActionSelect(ComN,comArgs))
+        button.pack(side = Side)
 
-        #add in return option like rectangle, for reasons ?
+        if returnOn == True:
+            return button
+    def MoveObject(self,ID,nX,nY):
+            # use in conjunction with CreateRectangle with ReturnOn to get ID 
+            self.__canvas.coords(ID, nX, nY, nX + self.__Div, nY + self.__Div,)
+            self.__canvas.update()
 
     def GetWInfo(self):
         # returns window info since they are private
-        return (self.__wWidth,self.__wHeight,self.__Div)
+        return self.__wWidth, self.__wHeight, self.__Div
 
-    def MoveObject(self,ID,nX,nY):
-        # use in conjunction with CreateRectangle with ReturnOn to get ID 
-        self.__canvas.coords(ID, nX, nY, nX + self.__Div, nY + self.__Div,)
-        self.__canvas.update()
-    
-def DisplayMap(gui,MapList):
+    def GetCanvas(self):
+        return self.__canvas
+
+    def GetMainFrame(self):
+        return self.__frame
+
+def ReadSplit(filePath):
+    """Opens specified file, reads it and splits into single symbols"""
+
+    # Read and close
+    file = open(filePath,"r")
+    string = file.read()
+    file.close()
+    # --------------
+
+    # Splitting
+    Pre = string.rsplit("\n")
+    Final = []
+
+    for i in Pre:
+        Final.append(i.split())
+    # ---------
+
+    return Final
+
+def DisplayMap(gui,mapList):
     # for ease of use
     wData = gui.GetWInfo()
     w = wData[0]
     h = wData[1]
     d = wData[2]
+    # ---------------
 
+    # Places all the rectangles
     for i in range(int(h / d)):
         y = i * d
         for j in range(int(w / d)):
             x = j * d
             
-            if MapList[i][j] == "1":
+            if mapList[i][j] == "1":
                 gui.CreateRectangle(x,y,"gray","gray")
-            elif MapList[i][j] == "2":
+            elif mapList[i][j] == "2":
                 gui.CreateRectangle(x,y,"green","green")
-            elif MapList[i][j] == "3":
+            elif mapList[i][j] == "3":
                 gui.CreateRectangle(x,y,"red","red")
+            elif mapList[i][j] == "4":
+                gui.CreateRectangle(x,y,"blue","blue")
             else:
                 raise ValueError("Unidentified symbol was found in MapList")
 
 def main():
 
+    # Setup GUI
     root = Tk()
-    gui = GUI(root)
+    gui = GUI(root) # DO NOT USE root past this point
+    # ---------
 
-    MapList = ReadSplit("Map.txt")
-    
-    DisplayMap(gui,MapList)
+    # Reads and displays map
+    mapList = ReadSplit("Map.txt")
+    DisplayMap(gui,mapList)
+    # ----------------------
 
-    # make cat into class go with OOP (object oriented programing)
-    Cat = gui.CreateRectangle(100,100,"orange","orange",True)
-    gui.CreateButton("Move",1,(Cat,0,0))
+    # Make cat into class(OOP)
+    cat = gui.CreateRectangle(100,100,"orange","orange",True)
+    gui.CreateButton("Move",1,(cat,0,0))
 
+    # Mainloop, MUST ALWAYS BE ON BOTTOM
     root.mainloop()
-
+    
 if __name__ == "__main__":
 	sys.exit(main())
