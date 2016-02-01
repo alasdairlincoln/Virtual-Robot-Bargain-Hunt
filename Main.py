7b1d4a75f68be10d316e209140fe821fad828e6f
Drakoshi <taduksaz@live.com>
2016-02-01 10:40:14 +0000

    Changes and Key press detects

--- Main.py
+++ Main.py
@@ -17,20 +17,17 @@ class GUI:
 
     def __init__(self,root):
         self.__root = root
-        self.__frame = Frame(self.__root).pack()
+        self.__frame = Frame(self.__root)
+        self.__frame.pack()
 
-        # change window info here!
         self.__root.title("Cat Hunt")
-        self.__wWidth = 500
-        self.__wHeight = 500
-        self.__Div = 50
-        # ------------------------
 
-        # Insert start/main menu here? make it return some stuff to here?
-
-        # Canvas for map
-        self.__canvas = Canvas(self.__frame,width = self.__wWidth, height = self.__wHeight, bg = "white")
+    def CreateCanvas(self,wWidth,wHeight,Div,background = "white"):
+        # Creates Canvas
+        self.__canvas = Canvas(self.__frame,width = wWidth,height = wHeight,bg = background)
         self.__canvas.pack()
+        self.__Div = Div
+
 
     def CreateRectangle(self,x,y,Fill = "white",Outline = "black", returnOn = False):
         # Wrapper for create rectangle function
@@ -41,28 +38,13 @@ class GUI:
         if returnOn == True:
             return rect
 
-    def __ActionSelect(self,number,pArgs):
-        # used as a place holder to get other commands, since i failed to pass in a command :| + google suggested this :D 
-        if number == 0:
-            # default case in case nothing specified
-            pass
-        elif number == 1:
-            # unpack and pass arguments 
-            ID = pArgs[0]
-            x = pArgs[1]
-            y = pArgs[2]
-            self.MoveObject(ID,x,y)
-
-    def CreateButton(self, txt, ComN = 0, comArgs = (), Side = "top", returnOn = False):
-        button = Button(self.__frame,text = txt,command = lambda: self.__ActionSelect(ComN,comArgs))
-        button.pack(side = Side)
-
-        if returnOn == True:
-            return button
     def MoveObject(self,ID,nX,nY):
             # use in conjunction with CreateRectangle with ReturnOn to get ID 
             self.__canvas.coords(ID, nX, nY, nX + self.__Div, nY + self.__Div,)
             self.__canvas.update()
+
+    def Callback(self,event):
+        print("pressed " + str(event.keycode))
 
     def GetWInfo(self):
         # returns window info since they are private
@@ -73,6 +55,12 @@ class GUI:
 
     def GetMainFrame(self):
         return self.__frame
+
+    def ClearFrame(self):
+        # As name suggests, cleats the main frame
+        self.__frame.destroy()
+        self.__frame = Frame(self.__root)
+        self.__frame.pack()
 
 def ReadSplit(filePath):
     """Opens specified file, reads it and splits into single symbols"""
@@ -93,15 +81,8 @@ def ReadSplit(filePath):
 
     return Final
 
-def DisplayMap(gui,mapList):
-    # for ease of use
-    wData = gui.GetWInfo()
-    w = wData[0]
-    h = wData[1]
-    d = wData[2]
-    # ---------------
+def DisplayMap(gui,mapList,h,w,d):
 
-    # Places all the rectangles
     for i in range(int(h / d)):
         y = i * d
         for j in range(int(w / d)):
@@ -120,19 +101,29 @@ def DisplayMap(gui,mapList):
 
 def main():
 
+    mainCanvasWidth = 500
+    mainCanvasHeight = 500
+    mainCanvasDiv = 50
+
     # Setup GUI
     root = Tk()
     gui = GUI(root) # DO NOT USE root past this point
+    gui.CreateCanvas(mainCanvasWidth,mainCanvasHeight,mainCanvasDiv,"black")
     # ---------
 
     # Reads and displays map
     mapList = ReadSplit("Map.txt")
-    DisplayMap(gui,mapList)
+    DisplayMap(gui,mapList,mainCanvasWidth,mainCanvasHeight,mainCanvasDiv)
     # ----------------------
 
     # Make cat into class(OOP)
     cat = gui.CreateRectangle(100,100,"orange","orange",True)
-    gui.CreateButton("Move",1,(cat,0,0))
+    frame = gui.GetMainFrame()
+    button = Button(frame,text = "Move",command = lambda: gui.MoveObject(cat,0,0))
+    button.pack()
+
+    root.bind("<KeyRelease>",gui.Callback)
+    frame.pack()
 
     # Mainloop, MUST ALWAYS BE ON BOTTOM
     root.mainloop()
